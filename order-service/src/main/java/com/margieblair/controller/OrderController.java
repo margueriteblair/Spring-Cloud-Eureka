f4070d82d2fabfae7b44c85dd6ba59ab23428f5f
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-21 12:09:05 -0400

    post mapping

--- order-service/src/main/java/com/margieblair/controller/OrderController.java
+++ order-service/src/main/java/com/margieblair/controller/OrderController.java
@@ -2,12 +2,22 @@ package com.margieblair.controller;
 
 
 import com.margieblair.service.OrderService;
+import main.java.com.margieblair.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
 
     @Autowired //inject
     private OrderService orderService;
+
+    @PostMapping("bookOrder")
+    public Order bookOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
 }
