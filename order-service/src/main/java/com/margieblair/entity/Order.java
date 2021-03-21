ccea4d27f7008beedf4199b57fb8b8808e62a48e
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-20 23:31:53 -0400

    order entity

---
+++ order-service/src/main/java/com/margieblair/entity/Order.java
@@ -0,0 +1,12 @@
+package main.java.com.margieblair.entity;
+
+
+@Entity
+@Table
+public class Order {
+
+    private int id;
+    private String name;
+    private int qty;
+    private double price;
+}
