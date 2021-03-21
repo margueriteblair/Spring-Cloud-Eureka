403bf537bca4c550241f39e76500eb22ae036943
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-20 23:57:43 -0400

    service

---
+++ order-service/src/main/java/com/margieblair/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.margieblair.service;
+
+
+import com.margieblair.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public main.java.com.margieblair.entity.Order saveOrder(main.java.com.margieblair.entity.Order xs)
+}
