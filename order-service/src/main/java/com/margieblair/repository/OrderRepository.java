314ee2397e66fcb55dac58b581d3d949e8cc6bcd
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-20 23:55:48 -0400

    look ma i made a repository

---
+++ order-service/src/main/java/com/margieblair/repository/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.margieblair.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<main.java.com.margieblair.entity.Order, Integer> {
+}
