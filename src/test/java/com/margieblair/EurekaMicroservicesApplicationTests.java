9726fda1e426d380edfdd9aacde3163619481215
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-20 22:54:21 -0400

    the whole shebang

---
+++ src/test/java/com/margieblair/EurekaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margieblair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
